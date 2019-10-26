eec7331e91713e52daaf02633da1bfe467a6f663
crazyrabbitLTC <dennison@dennisonbertram.com>
2019-10-26 16:09:29 -0400

    Working version.

--- contracts/MerkleMintCore.sol
+++ contracts/MerkleMintCore.sol
@@ -15,14 +15,10 @@ contract MerkleMintCore is
     ERC721MetadataMintable,
     ERC721Pausable
 {
-
     uint256 private _currentTokenId;
     string public tokenURIBase;
 
-    function initialize(
-        address[] memory minters,
-        address[] memory pausers
-    ) public initializer {
+    function initialize(address[] memory minters, address[] memory pausers) public initializer {
         _currentTokenId = 0;
         ERC721.initialize();
         ERC721Enumerable.initialize();
