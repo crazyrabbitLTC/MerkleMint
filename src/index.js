f3653e93c63aa630aeddb1dca90f3c4dc2900491
crazyrabbitLTC <dennison@dennisonbertram.com>
2019-11-11 08:29:00 -0500

    extract some functions to external file

--- src/index.js
+++ src/index.js
@@ -3,21 +3,11 @@ const {getFileType} = require("./utils/getFileType")
 const {extractEXIF} = require("./utils/extractEXIF")
 const fs = require("fs")
 const path = require("path")
-const uuidv4 = require("uuid/v4")
 const {config} = require("./config")
-const web3 = require("web3")
 const {MerkleTree} = require("./utils/merkleTree")
 const {keccak256, bufferToHex} = require("ethereumjs-util")
-const {openSea, cmoa, exif, imageInfo, artist, chainData, dao} = require("./exampleMetaData")
-const ipfsClient = require("ipfs-http-client")
-
-const ipfs = ipfsClient(process.env.INFURA_IPFS, {protocol: "https"})
-
-const asyncForEach = async (array, callback) => {
-    for (let index = 0; index < array.length; index++) {
-        await callback(array[index], index, array)
-    }
-}
+const {openSea} = require("./exampleMetaData")
+const {sendMetaDataToIPFS, sendFilesToIPFS} = require("./utils/ipfsUpload")
 
 const start = async () => {
     //Get Directory Contents
@@ -47,63 +37,9 @@ const start = async () => {
         }
     })
 
-    const sendFilesToIPFS = async filesWithMetaData => {
-        let newArray = []
-
-        try {
-            await asyncForEach(filesWithMetaData, async fileObj => {
-                let id
-                try {
-                    id = await ipfs.addFromFs(fileObj.filePath)
-                } catch (error) {
-                    console.log(error)
-                }
-
-                newArray.push({
-                    ...fileObj,
-                    image: `/ipfs/${id[0].hash}`,
-                })
-            })
-        } catch (error) {
-            console.log(error)
-        }
-
-        return newArray
-    }
-
-    const sendMetaDataToIPFS = async filesWithHash => {
-        let newArray = []
-
-        try {
-            await asyncForEach(filesWithHash, async fileObj => {
-                let id
-
-                try {
-                    id = await ipfs.add(Buffer.from(JSON.stringify(fileObj)))
-
-                    newArray.push({
-                        ...fileObj,
-                        id: {
-                            tokenURI: id[0].hash,
-                            ipfs: id[0],
-                            hashURI: web3.utils.soliditySha3(id[0].hash),
-                        },
-                    })
-                } catch (error) {
-                    throw new Error(error)
-                }
-            })
-        } catch (error) {
-            throw new Error(error)
-        }
-        return newArray
-    }
-
     const filesWithHash = await sendFilesToIPFS(filesWithMetaData)
 
     const fileMetaDataWithIPFSHash = await sendMetaDataToIPFS(filesWithHash)
-
-    console.log(JSON.stringify(fileMetaDataWithIPFSHash, null, 4))
 
     const merkleTree = new MerkleTree(fileMetaDataWithIPFSHash.map(fileObj => fileObj.id.tokenURI))
 
@@ -120,7 +56,10 @@ const start = async () => {
         }
     })
 
-    //console.log(JSON.stringify(filesWithProofs, null, 4))
+    console.log(JSON.stringify(filesWithProofs, null, 4))
+    fs.writeFileSync(path.join(config.path, "output.json"), JSON.stringify(filesWithProofs))
+
+    process.exit(0)
 }
 
 start()
