3727403c3fa75701ef716d501a79b0cf6932ab5d
crazyrabbitLTC <dennison@dennisonbertram.com>
2019-11-10 22:31:57 -0500

    Add IPFS uploading

--- src/index.js
+++ src/index.js
@@ -1,18 +1,23 @@
-require('dotenv').config()
-const { getFileType } = require("./utils/getFileType")
-const { extractEXIF } = require("./utils/extractEXIF")
+require("dotenv").config()
+const {getFileType} = require("./utils/getFileType")
+const {extractEXIF} = require("./utils/extractEXIF")
 const fs = require("fs")
 const path = require("path")
 const uuidv4 = require("uuid/v4")
 const {config} = require("./config")
 const web3 = require("web3")
 const {MerkleTree} = require("./utils/merkleTree")
-const { keccak256, bufferToHex } = require('ethereumjs-util');
-const { openSea, cmoa, exif, imageInfo, artist, chainData, dao } = require('./exampleMetaData');
-const ipfsClient = require('ipfs-http-client');
+const {keccak256, bufferToHex} = require("ethereumjs-util")
+const {openSea, cmoa, exif, imageInfo, artist, chainData, dao} = require("./exampleMetaData")
+const ipfsClient = require("ipfs-http-client")
 
-const ipfs = ipfsClient(process.env.INFURA_IPFS, {protocol: 'https'});
+const ipfs = ipfsClient(process.env.INFURA_IPFS, {protocol: "https"})
 
+const asyncForEach = async (array, callback) => {
+    for (let index = 0; index < array.length; index++) {
+        await callback(array[index], index, array)
+    }
+}
 
 const start = async () => {
     //Get Directory Contents
@@ -27,7 +32,6 @@ const start = async () => {
         return config.imageTypes.includes(getFileType(file.filePath).ext)
     })
 
-
     const filesWithMetaData = validFiles.map(fileObj => {
         return {
             ...fileObj,
@@ -38,65 +42,85 @@ const start = async () => {
                 imageInfo: [],
                 artist: [],
                 chainData: [],
-                dao: []
+                dao: [],
             },
         }
     })
 
-    const asyncForEach = async (array, callback) => {
-      for (let index = 0; index < array.length; index++) {
-        await callback(array[index], index, array);
-      }
-    }
+    const sendFilesToIPFS = async filesWithMetaData => {
+        let newArray = []
 
-    const sendFilesToIPFS = async (filesWithMetaData)=>{
-      let newArray = [];
+        try {
+            await asyncForEach(filesWithMetaData, async fileObj => {
+                let id
+                try {
+                    id = await ipfs.addFromFs(fileObj.filePath)
+                } catch (error) {
+                    console.log(error)
+                }
 
-      try {
-        await asyncForEach(filesWithMetaData, async (fileObj) => {
-          let id
-           try {
-            id = await ipfs.addFromFs(fileObj.filePath);
-          } catch (error) {
+                newArray.push({
+                    ...fileObj,
+                    image: `/ipfs/${id[0].hash}`,
+                })
+            })
+        } catch (error) {
             console.log(error)
-          }
-          
-          newArray.push({
-            ...fileObj,
-            id: {
-              tokenURI: id[0].hash,
-              ipfs: id,
-              hashURI: web3.utils.soliditySha3(id[0].hash)
-            }
-          })
-        })
-      } catch (error) {
-        console.log(error)
-      }
-       
-      return newArray;
-    }
-    
-    const filesWithHash = await sendFilesToIPFS(filesWithMetaData);
-
-    console.log(filesWithHash);
-    const merkleTree = new MerkleTree(filesWithHash.map(fileObj => fileObj.id.tokenURI))
-
-    const root = merkleTree.getHexRoot();
-
-    const filesWithProofs = filesWithHash.map(fileObj => {
-      return {
-        ...fileObj,
-        merkleProof: {
-          root: root,
-          proof: merkleTree.getHexProof(fileObj.id.tokenURI),
-          leaf: bufferToHex(keccak256(fileObj.id.tokenURI))
         }
-      }
+
+        return newArray
+    }
+
+    const sendMetaDataToIPFS = async filesWithHash => {
+        let newArray = []
+
+        try {
+            await asyncForEach(filesWithHash, async fileObj => {
+                let id
+
+                try {
+                    id = await ipfs.add(Buffer.from(JSON.stringify(fileObj)))
+
+                    newArray.push({
+                        ...fileObj,
+                        id: {
+                            tokenURI: id[0].hash,
+                            ipfs: id[0],
+                            hashURI: web3.utils.soliditySha3(id[0].hash),
+                        },
+                    })
+                } catch (error) {
+                    throw new Error(error)
+                }
+            })
+        } catch (error) {
+            throw new Error(error)
+        }
+        return newArray
+    }
+
+    const filesWithHash = await sendFilesToIPFS(filesWithMetaData)
+
+    const fileMetaDataWithIPFSHash = await sendMetaDataToIPFS(filesWithHash)
+
+    console.log(JSON.stringify(fileMetaDataWithIPFSHash, null, 4))
+
+    const merkleTree = new MerkleTree(fileMetaDataWithIPFSHash.map(fileObj => fileObj.id.tokenURI))
+
+    const root = merkleTree.getHexRoot()
+
+    const filesWithProofs = fileMetaDataWithIPFSHash.map(fileObj => {
+        return {
+            ...fileObj,
+            merkleProof: {
+                root: root,
+                proof: merkleTree.getHexProof(fileObj.id.tokenURI),
+                leaf: bufferToHex(keccak256(fileObj.id.tokenURI)),
+            },
+        }
     })
 
     //console.log(JSON.stringify(filesWithProofs, null, 4))
-
 }
 
 start()
