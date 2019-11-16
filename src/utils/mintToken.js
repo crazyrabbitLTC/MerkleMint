49bb8cb84dcbc40dd70c8f312143607cbd0e17d2
crazyrabbitLTC <dennison@dennisonbertram.com>
2019-11-16 13:47:28 -0500

    Mint all tokens in array and save ZOS project json

--- src/utils/mintToken.js
+++ src/utils/mintToken.js
@@ -1,11 +1,14 @@
 const MyWeb3 = require("web3")
+const fs = require("fs")
+const path = require("path")
 // const web3 = new Web3("ws://127.0.0.1:8545")
 
 global.artifacts = artifacts
 global.web3 = web3
 
 const {Contracts, SimpleProject, ZWeb3} = require("@openzeppelin/upgrades")
-
+const configPath = "../../../../Desktop/uploadTest1/"
+const config = require("../../../../Desktop/uploadTest1/treeData.json")
 const token = {
     name: "TestToken",
     symbol: "TT",
@@ -20,7 +23,7 @@ async function main() {
     const [creatorAddress, initializerAddress, additionalMinter] = await ZWeb3.accounts()
 
     const MerkleMint = new SimpleProject("MerkleMint", null, {from: creatorAddress})
-    console.log(MerkleMint)
+    //console.log(MerkleMint)
 
     const MMCoreInstance = await MerkleMint.createProxy(MerkleMintCore, {
         initArgs: [token.name, token.symbol, [initializerAddress], [initializerAddress]],
@@ -32,26 +35,26 @@ async function main() {
         .addMinter(MMControllerInstance.address)
         .send({from: initializerAddress, gas: 1500000, gasPrice: "30000000000000"})
 
-    console.log(
-        "Is MMController minter?",
-        await MMCoreInstance.methods
-            .isMinter(MMControllerInstance.address)
-            .call({from: additionalMinter}),
-    )
+    // console.log(
+    //     "Is MMController minter?",
+    //     await MMCoreInstance.methods
+    //         .isMinter(MMControllerInstance.address)
+    //         .call({ from: additionalMinter }),
+    // )
 
     //Add Token address to MMController
     await MMControllerInstance.methods
         .initializeController(MMCoreInstance.address)
         .send({from: initializerAddress, gas: 1500000, gasPrice: "30000000000000"})
 
-    console.log(
-        "MMController Owner: ",
-        await MMControllerInstance.methods.owner().call({from: initializerAddress}),
-        "Initialzier: ",
-        initializerAddress,
-    )
+    // console.log(
+    //     "MMController Owner: ",
+    //     await MMControllerInstance.methods.owner().call({ from: initializerAddress }),
+    //     "Initialzier: ",
+    //     initializerAddress,
+    // )
 
-    const {assets} = require("../../../../Desktop/uploadTest1/treeData.json")
+    const {assets} = config
     const {tokenId, tokenURI, hashOfURI, root, leaf, proof} = assets[0]
     const serieName = "Series 1"
     const serieNumber = 0
@@ -62,7 +65,7 @@ async function main() {
     let gas = await MMControllerInstance.methods
         .addSerie(serieNumber, root, serieName, hashOfURI)
         .estimateGas({from: initializerAddress})
-    console.log("Gas: ", gas)
+    //console.log("Gas: ", gas)
 
     let lastBlock = await web3.eth.getBlock("latest")
     let limit = lastBlock.gasLimit
@@ -77,14 +80,14 @@ async function main() {
     //console.log("TokenURI of 0: ", await MMCoreInstance.methods.tokenURI(0).call({from: initializerAddress}))
 
     let balanceAfter = await web3.eth.getBalance(initializerAddress)
-    console.log(typeof balanceAfter)
-    console.log(
-        "The cost of creating a serie was: ",
-        MyWeb3.utils.fromWei(
-            (parseInt(balanceBefore) - parseInt(balanceAfter)).toString(),
-            "ether",
-        ),
-    )
+    // console.log(typeof balanceAfter)
+    // console.log(
+    //     "The cost of creating a serie was: ",
+    //     MyWeb3.utils.fromWei(
+    //         (parseInt(balanceBefore) - parseInt(balanceAfter)).toString(),
+    //         "ether",
+    //     ),
+    // )
 
     gas = await MMControllerInstance.methods
         .mintAsset(tokenURI, leaf, proof, tokenId, serieNumber)
@@ -93,19 +96,32 @@ async function main() {
     gas = Math.min(limit - 1, Math.ceil(gas * 1.2))
     gasPrice = await web3.eth.getGasPrice()
 
-    await MMControllerInstance.methods
-        .mintAsset(tokenURI, leaf, proof, tokenId, serieNumber)
-        .send({from: initializerAddress, gas, gasPrice})
+    let promiseArray = []
+    for (item of assets) {
+        const {tokenId, tokenURI, hashOfURI, root, leaf, proof} = item
+        promiseArray.push(
+            await MMControllerInstance.methods
+                .mintAsset(tokenURI, leaf, proof, tokenId, serieNumber)
+                .send({from: initializerAddress, gas, gasPrice}),
+        )
+    }
 
-    console.log(
-        "TokenURI of 0: ",
-        await MMCoreInstance.methods.tokenURI(0).call({from: initializerAddress}),
+    const networkId = await web3.eth.net.getId()
+    fs.writeFileSync(
+        path.join(configPath, `Project-${networkId}.json`),
+        JSON.stringify(MerkleMint, null, 4),
     )
+    Promise.all(promiseArray).then(x => console.log(x))
 
-    console.log("MMController Info: ", MMControllerInstance.address)
+    // console.log(
+    //     "TokenURI of 0: ",
+    //     await MMCoreInstance.methods.tokenURI(0).call({ from: initializerAddress }),
+    // )
 
-    //Save the project JSON
-    console.log(MerkleMint)
+    // console.log("MMController Info: ", MMControllerInstance.address)
+
+    // //Save the project JSON
+    // console.log(MerkleMint)
 }
 
 // For truffle exec
